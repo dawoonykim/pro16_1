d2ae392d03996ec95bb41a50c8313a0b5c322294
ramykim <dawoonk111@gmail.com>
2024-03-07 17:44:15 +0900

    pro16 2024.03.07 수업내용

---
+++ src/main/java/sec01/ex01/ajaxTest2.java
@@ -0,0 +1,41 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/replyTest1")
+public class ajaxTest2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("ajax 실행중...");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		doHandle(request, response);
+	}
+
+	protected void doHandle(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String toClient=request.getParameter("type");
+		System.out.println(toClient);
+	
+		PrintWriter pw=response.getWriter();
+		pw.write(toClient);
+	}
+
+}
